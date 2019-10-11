8044a698adc92d28ef96c3dfcbb705e9a2726771
febbyepermana <50956663+febbyepermana@users.noreply.github.com>
2019-10-11 23:29:52 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 	<h1 align="center" style="background-color:tomato ;""color: white" > Daftar Anggota</h1><br><br>
 
 	<center><a href="https://tugas1-101163xx-24.herokuapp.com/" class='tombol'>Anggota 1</a><br><br><br><br>
-	<a href="#" class='tombol'>Anggota 2</a><br><br><br><br>
-	<a href="#" class='tombol'>Anggota 3</a><br><br><br><br>
+	<a href="https://tugas1-101163xx-32.herokuapp.com/" class='tombol'>Anggota 2</a><br><br><br><br>
+	<a href="https://tugas1-101162xx-14.herokuapp.com/" class='tombol'>Anggota 3</a><br><br><br><br>
 	<a href="#" class='tombol'>Anggota 4</a><br><br><br><br>
 	<a href="#" class='tombol'>Anggota 5</a><br><br><br><br>
 </center>
