c7c19b66cdfa4045cd6758955704db9b4268bd3d
febbyepermana <50956663+febbyepermana@users.noreply.github.com>
2019-10-12 11:00:19 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
 	<center><a href="https://tugas1-101163xx-24.herokuapp.com/" class='tombol'>Anggota 1</a><br><br><br><br>
 	<a href="https://tugas1-101163xx-32.herokuapp.com/" class='tombol'>Anggota 2</a><br><br><br><br>
 	<a href="https://tugas1-101162xx-14.herokuapp.com/" class='tombol'>Anggota 3</a><br><br><br><br>
-	<a href="#" class='tombol'>Anggota 4</a><br><br><br><br>
-	<a href="#" class='tombol'>Anggota 5</a><br><br><br><br>
+	<a href="https://tugas1-101163xx-30.herokuapp.com/" class='tombol'>Anggota 4</a><br><br><br><br>
+	<a href="https://tugas1-101163xx-34.herokuapp.com/" class='tombol'>Anggota 5</a><br><br><br><br>
 </center>
 
 </body>
