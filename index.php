ac0fa37123b250d8239d663263b2a4d9f96ec1eb
febbyepermana <50956663+febbyepermana@users.noreply.github.com>
2019-10-12 11:04:32 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="tombol.css">
 	<title>
-		101163XX-24
+		Beranda Kelompok 5
 	</title>
 </head>
 <body bgcolor="mediumseagreen">
 
-	<h1 align="center" style="background-color:tomato ;""color: white" > Daftar Anggota</h1><br><br>
+	<h1 align="center" style="background-color:tomato ;""color: white" > Daftar Anggota Kelompok 5</h1><br><br>
 
 	<center><a href="https://tugas1-101163xx-24.herokuapp.com/" class='tombol'>Anggota 1</a><br><br><br><br>
 	<a href="https://tugas1-101163xx-32.herokuapp.com/" class='tombol'>Anggota 2</a><br><br><br><br>
